00f8a8e218c88ca2b424714cf6c769c3c2062bbb
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-06-21 23:48:29 +0000

    added type of domain extension

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let pronouns = ["the", "our"];
 let adjs = ["great", "big"];
 let nouns = ["jogger", "racoon"];
+let domains = [".com", ".net", ".us", ".io"];
 
 pronouns.forEach((pronoun) => {
   adjs.forEach((adj) => {
     nouns.forEach((noun) => {
-      console.log(pronoun + adj + noun + ".com");
+      domains.forEach((domain) => {
+        console.log(pronoun + adj + noun + domain);
+      });
     });
   });
 });
