33bc65814babcad26c2ed5568733ac9db4bbf929
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-06-21 23:29:40 +0000

    final

---
+++ app.js
@@ -0,0 +1,11 @@
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+pronouns.forEach((pronoun) => {
+  adjs.forEach((adj) => {
+    nouns.forEach((noun) => {
+      console.log(pronoun + adj + noun + ".com");
+    });
+  });
+});
